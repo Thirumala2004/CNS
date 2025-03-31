e4fc9417f4de3c9815444d42aa032014e76e3608
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2025-03-31 11:23:56 +0530

    Update OneTimePad.java

--- OneTimePad.java
+++ OneTimePad.java
@@ -1,191 +1,53 @@
-import java.util.*; 
-class import java.util.*; 
-class OneTimePad{ 
-    public static void main(String args[])throws Exception { 
-        String plaintext,key; 
-        Scanner scn=new Scanner(System.in); 
-        System.out.println("Enter plaintext:"); 
-        plaintext=scn.nextLine(); 
-        System.out.println("Enter key:"); 
-        key=scn.nextLine(); 
-        OneTimePadCipherImplementation OneTimePad=new OneTimePadCipherImplementation(); 
-        String ciphertext=OneTimePad.Encryption(plaintext,key);
-        System.out.println("Encrpted   text    is:"+ciphertext);
-        System.out.println("Decrypted text is:"+OneTimePad.Decryption(ciphertext,key)); 
-    } 
+import java.io.*;
+import java.util.*;
+public class OneTimePad {
+	public static String stringEncryption(String text,String key)
+	{
+
+		String cipherText = "";
+		int cipher[] = new int[key.length()];
+		for (int i = 0; i < key.length(); i++) {
+			cipher[i] = text.charAt(i) - 'A'+ key.charAt(i)- 'A';
+		}
+		for (int i = 0; i < key.length(); i++) {
+			if (cipher[i] > 25) {
+				cipher[i] = cipher[i] - 26;
+			}
+		}
+		for (int i = 0; i < key.length(); i++) {
+			int x = cipher[i] + 'A';
+			cipherText += (char)x;
+		}
+		return cipherText;
+	}
+	public static String stringDecryption(String s,String key)
+	{
+		String plainText = "";
+		int plain[] = new int[key.length()];
+		for (int i = 0; i < key.length(); i++) {
+			plain[i]= s.charAt(i) - 'A'- (key.charAt(i) - 'A');
+		}
+		for (int i = 0; i < key.length(); i++) {
+			if (plain[i] < 0) {
+				plain[i] = plain[i] + 26;
+			}
+		}
+		for (int i = 0; i < key.length(); i++) {
+			int x = plain[i] + 'A';
+			plainText += (char)x;
+		}
+		return plainText;
+	}
+	public static void main(String[] args)
+	{
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter message: ");
+		String plainText = sc.next();
+		System.out.print("Enter key: ");
+		String key = sc.next();
+		String encryptedText = stringEncryption(plainText.toUpperCase(), key.toUpperCase());
+		System.out.println("Cipher Text - "+ encryptedText);
+		System.out.println("Message - "+ stringDecryption(encryptedText,key.toUpperCase()));
+	}
 }
-class OneTimePadCipherImplementation{ 
-    String Encryption(String plaintext,String key) { 
-        plaintext=plaintext.toLowerCase(); 
-        msg m1=new msg(); 
-        int pt[]=new int[plaintext.length()]; 
-        int k[]=new int[key.length()]; 
-        int ct[]=new int[plaintext.length()]; 
-        for(int i=0;i < plaintext.length();i++) { 
-            pt[i]=m1.Ipos(plaintext.charAt(i)); 
-        } 
-        for(int i=0;i < key.length();i++) { 
-            k[i]=m1.Ipos(key.charAt(i)); 
-        } 
-        int j=0; 
-        for(int i=0;i < plaintext.length();i++) { 
-            ct[i]=pt[i]+k[j]; 
-            j++; 
-            if(j==key.length()) 
-                j=0; 
-            if(ct[i]>26) 
-                ct[i]=ct[i]%26; 
-        } 
-        String cipher=""; 
-        for(int i=0;i < plaintext.length();i++) { 
-            cipher+=m1.Cpos(ct[i]); 
-        } 
-        return cipher; 
-    } 
-    String Decryption(String ciphertext,String key) { 
-        String plaintext=""; 
-        msg m1=new msg(); 
-        int pt[]=new int[ciphertext.length()]; 
-        int k[]=new int[key.length()]; 
-        int ct[]=new int[ciphertext.length()]; 
-        for(int i=0;i < ciphertext.length();i++) { 
-            ct[i]=m1.Ipos(ciphertext.charAt(i)); 
-        } 
-        for(int i=0;i < key.length();i++) { 
-            k[i]=m1.Ipos(key.charAt(i)); 
-        } 
-        int j=0; 
-        for(int i=0;i < ciphertext.length();i++) { 
-            pt[i]=ct[i]-k[j]; 
-            j++; 
-            if(j==key.length()) 
-                j=0; 
-            if(pt[i] < 0) 
-                pt[i]+=26; 
-        } 
-        String cipher=""; 
-        for(int i=0;i < ciphertext.length();i++){
-            plaintext+=m1.Cpos(pt[i]);
-        } 
-        return plaintext; 
-    } 
-} 
-class msg{ 
-    int a=97; 
-    char all[]=new char[27]; 
-    msg() { 
-        for(int i=0;i<26;i++) { 
-            all[i]=(char)a; 
-            a++;
-        } 
-    } 
-    int Ipos(char c) { 
-        int i=0; 
-        for(;i<26;i++) { 
-            if(all[i]==c) { 
-                break;
-            } 
-        } 
-        return i; 
-    } 
-    char Cpos(int c) { 
-        int i=0; 
-        for(;i<c;i++) { 
-        } 
-        return all[i]; 
-    } 
-}{ 
-    public static void main(String args[])throws Exception { 
-        String plaintext,key; 
-        Scanner scn=new Scanner(System.in); 
-        System.out.println("Enter plaintext:"); 
-        plaintext=scn.nextLine(); 
-        System.out.println("Enter key:"); 
-        key=scn.nextLine(); 
-        OneTimePadCipherImplementation OneTimePad=new OneTimePadCipherImplementation(); 
-        String ciphertext=OneTimePad.Encryption(plaintext,key);
-        System.out.println("Encrpted   text    is:"+ciphertext);
-        System.out.println("Decrypted text is:"+OneTimePad.Decryption(ciphertext,key)); 
-    } 
-}
-class OneTimePadCipherImplementation{ 
-    String Encryption(String plaintext,String key) { 
-        plaintext=plaintext.toLowerCase(); 
-        msg m1=new msg(); 
-        int pt[]=new int[plaintext.length()]; 
-        int k[]=new int[key.length()]; 
-        int ct[]=new int[plaintext.length()]; 
-        for(int i=0;i < plaintext.length();i++) { 
-            pt[i]=m1.Ipos(plaintext.charAt(i)); 
-        } 
-        for(int i=0;i < key.length();i++) { 
-            k[i]=m1.Ipos(key.charAt(i)); 
-        } 
-        int j=0; 
-        for(int i=0;i < plaintext.length();i++) { 
-            ct[i]=pt[i]+k[j]; 
-            j++; 
-            if(j==key.length()) 
-                j=0; 
-            if(ct[i]>26) 
-                ct[i]=ct[i]%26; 
-        } 
-        String cipher=""; 
-        for(int i=0;i < plaintext.length();i++) { 
-            cipher+=m1.Cpos(ct[i]); 
-        } 
-        return cipher; 
-    } 
-    String Decryption(String ciphertext,String key) { 
-        String plaintext=""; 
-        msg m1=new msg(); 
-        int pt[]=new int[ciphertext.length()]; 
-        int k[]=new int[key.length()]; 
-        int ct[]=new int[ciphertext.length()]; 
-        for(int i=0;i < ciphertext.length();i++) { 
-            ct[i]=m1.Ipos(ciphertext.charAt(i)); 
-        } 
-        for(int i=0;i < key.length();i++) { 
-            k[i]=m1.Ipos(key.charAt(i)); 
-        } 
-        int j=0; 
-        for(int i=0;i < ciphertext.length();i++) { 
-            pt[i]=ct[i]-k[j]; 
-            j++; 
-            if(j==key.length()) 
-                j=0; 
-            if(pt[i] < 0) 
-                pt[i]+=26; 
-        } 
-        String cipher=""; 
-        for(int i=0;i < ciphertext.length();i++){
-            plaintext+=m1.Cpos(pt[i]);
-        } 
-        return plaintext; 
-    } 
-} 
-class msg{ 
-    int a=97; 
-    char all[]=new char[27]; 
-    msg() { 
-        for(int i=0;i<26;i++) { 
-            all[i]=(char)a; 
-            a++;
-        } 
-    } 
-    int Ipos(char c) { 
-        int i=0; 
-        for(;i<26;i++) { 
-            if(all[i]==c) { 
-                break;
-            } 
-        } 
-        return i; 
-    } 
-    char Cpos(int c) { 
-        int i=0; 
-        for(;i<c;i++) { 
-        } 
-        return all[i]; 
-    } 
-}
+
